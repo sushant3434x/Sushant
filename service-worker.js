7d23891a548e1bc254bf7dcb125c333c86deef91
sushant3434x <sushantsingh3434x@gmail.com>
2026-01-14 00:46:21 +0530

    Delete service-worker.js
    Signed-off-by: sushant3434x <sushantsingh3434x@gmail.com>

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const cacheName='neet720-v1';
-const filesToCache=['/','/index.html','/manifest.json','/icon192.png','/icon512.png','https://cdn.jsdelivr.net/npm/chart.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
-
